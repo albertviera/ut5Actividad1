29da26aaa111ad09e8af458d60240ff804dae5a6
albertviera <albberto_89@hotmail.com>
2016-04-09 14:46:09 +0100

    commit 7

---
+++ src/dam_ed04_actividad/Banquero.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+
+/**
+ *
+ * @author Albbe
+ */
+public class Banquero {
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    
+}
