9f60a658616fb8f3fc0051c341f4896946bc86b4
albertviera <albberto_89@hotmail.com>
2016-04-09 14:47:13 +0100

    commit 8

--- src/dam_ed04_actividad/Banquero.java
+++ src/dam_ed04_actividad/Banquero.java
@@ -12,6 +12,30 @@ package dam_ed04_actividad;
 public class Banquero {
     private String nombre;
     private String apellidos;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
     private String dni;
     
 }
