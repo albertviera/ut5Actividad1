54e4f0ecb0f2f8188f3bdb72cb55292fad0a1879
Alberto Viera <Alberto Viera>
2016-04-09 14:33:20 +0100

    Commit 5

--- src/dam_ed04_actividad/Empleado.java
+++ src/dam_ed04_actividad/Empleado.java
@@ -9,25 +9,33 @@ package dam_ed04_actividad;
  *
  * @author Albbe
  */
+
+//inserto comentarios para el siguiente commit
 public class Empleado {
     private String nombre;
     private String apellidos;
     private String Dni;
+    
+    //variables privadas de tipo string llamadas nombre,apellidos,dni
 
     public String getNombre() {
         return nombre;
+        //getNombre
     }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+        //setNombre
     }
 
     public String getApellidos() {
         return apellidos;
+        //getApellidos
     }
 
     public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
+        //setApellidos
     }
 
     public String getDni() {
